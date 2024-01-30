ed0987eaa5ad8bf7d4e02214512ced862949ecd9
Ben Klimchock <klimmy1115@gmail.com>
2024-01-30 15:40:50 -0600

    Setup meta tags in header

--- wp-content/themes/luminary-honors/header.php
+++ wp-content/themes/luminary-honors/header.php
@@ -6,9 +6,9 @@
 
 <!doctype html>
 
-<html>
+<html <?php language_attributes();?>>
     <head>
-        <meta charset="">
+        <meta charset="<?php bloginfo('charset');?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
