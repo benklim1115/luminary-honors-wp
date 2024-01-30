07473510a7a103c532bf79e80f462404c8e5b459
Ben Klimchock <klimmy1115@gmail.com>
2024-01-30 15:47:19 -0600

    Setup in index.php for our content

--- wp-content/themes/luminary-honors/index.php
+++ wp-content/themes/luminary-honors/index.php
@@ -3,8 +3,11 @@
  * 
 **/
 
-
 get_header();
-
-
+    if (have_posts()) : 
+        while (have_posts()) :
+            the_post();
+            //do our stuff
+        endwhile;
+    endif;    
 get_footer();
