8726a73ef48878ee81ef20eb1157b838a8daab2d
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 13:03:03 -0600

    Setting up content-none.php for if posts cannot be found

--- wp-content/themes/luminary-honors/template-parts/page/content-none.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-none.php
@@ -1,2 +1,17 @@
 <?php
-echo "From content none";
+/**
+ * Template part for displaying a message that posts cannot be found
+ */
+?>
+
+<section class="no-results not-found">
+    <header class="page-header">
+        <h1 class="page-title"><?php esc_html_e('Nothing Found', 'Luminary Honors');?></h1>
+    </header>
+</section>
+
+<div class="page-content">
+    <?php
+
+    ?>
+</div>
