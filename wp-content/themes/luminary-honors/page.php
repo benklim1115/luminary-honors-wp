d6b3b8fc888d5f8518e735ae4afa9889432b7e04
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 09:48:49 -0600

    Working on page.php content, will make up our template for all our pages

--- wp-content/themes/luminary-honors/page.php
+++ wp-content/themes/luminary-honors/page.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * Template for displaying all the pages
+ */
+
+get_header(); 
+?>
+
+<!--- Will need to plug in Hunter's pages here? --->
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <!--- loop to print out the page --->
+        <?php 
+            while (have_posts()) :
+                the_post();
+                 //will grab something like this content-page.php
+                get_template_part('template/parts/page/content', 'page');
+            endwhile;
+        ?>
+    </main>
+</div>
+
+<?php
+get_footer();
