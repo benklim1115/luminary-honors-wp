f89d9e79d4fa0adba6d4fa42dd386d43a3aa0ec4
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 14:57:48 -0600

    Setting up search page, using some of the archive.php as a starter. Search in index.php too

--- wp-content/themes/luminary-honors/search.php
+++ wp-content/themes/luminary-honors/search.php
@@ -1,1 +1,32 @@
 <?php
+/**
+ * Template for displaying search results.
+ */
+get_header();
+?>
+
+<div id="primary" class="search-content-area">
+    <main id="main" class="site-main">
+        <?php 
+            if (have_posts()) :
+                //start the loop
+                while (have_posts()) :
+                    the_post();
+                    get_template_part('template-parts/post/content');
+                endwhile;
+
+                //for pagination
+                echo paginate_links([
+                    'prev_text' => esc_html__('Prev', 'Luminary Honors'),
+                    'next_text' => esc_html__('Next', 'Luminary Honors')
+                ]);
+
+        else : 
+            get_template_part('template-parts/page/content', 'none');       
+        ?>
+        <?php endif; ?>
+    </main>
+</div>
+
+<php
+get_footer();
