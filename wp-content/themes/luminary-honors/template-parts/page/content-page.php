20d952d54ec001953b928aa6e46d45d665427ea6
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 10:39:25 -0600

    Logic for displaying edit button for verified users

--- wp-content/themes/luminary-honors/template-parts/page/content-page.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-page.php
@@ -25,4 +25,34 @@
             ));
         ?>
     </div>
+
+    <!--- Edit option for user --->
+    <!--- Display the edit link if user has the permissions to edit it --->
+    <?php if (get_edit_post_link()) :?>    
+        <footer class="entry-footer">
+            <?php 
+                //Takes 3 arguments
+                //sprintf inserts title into the placeholder
+                //wp_kses allows certain elements/attributes for link text, safety
+                //__(Edit) allows for translation, using Ninestars theme
+                edit_post_link(
+                    sprintf(
+                        wp_kses(
+                            /* translators: %s: Name of the current post. Only visible to screen readers */
+                            __('Edit <span class="screen-reader-text">%s</span>', 'ninestars'),
+                            array(
+                                'span' => array(
+                                    'class' => array(),
+                                ),
+                            )
+                        ),
+                        get_the_title()
+                    ),
+                    '<span class="edit-link">',
+                    '</span>'
+                );
+            ?>
+        </footer>
+    <?php endif; ?>
+
 </article>
