c68a6702fef148abf7a889fb969ad11fa5971d3b
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 12:04:29 -0600

    Setting up template for archive.php

--- wp-content/themes/luminary-honors/archive.php
+++ wp-content/themes/luminary-honors/archive.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * Template for displaying archive pages.
+ */
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php if (have_posts()) :?>
+            <header class="archive-page-header">
+                <?php 
+                    the_archive_title('<h1 class="archive-title">', '</h1>');
+                ?>
+            </header>
+        <?php endif; ?>
+    </main>
+</div>
+
+<php
+get_footer();
