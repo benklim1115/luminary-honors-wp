c36a864487d002611bc4c48bd6c7b99185f5fc3f
Ben Klimchock <klimmy1115@gmail.com>
2024-01-31 09:06:05 -0600

    Set up for single post content

--- wp-content/themes/luminary-honors/single.php
+++ wp-content/themes/luminary-honors/single.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * Single page template, displays an individual post
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php 
+        while(have_posts()) :
+            the_post();
+            //single post content
+        endwhile;
+        ?>
+    </main>
+</div>
+
+<?php
+get_footer();
