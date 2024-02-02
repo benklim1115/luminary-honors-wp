d23945cd88b7ee3199fcecb39eba0cf8b8212875
Ben Klimchock <klimmy1115@gmail.com>
2024-02-02 14:33:51 -0600

    Double checking theme setup functionality

--- wp-content/themes/luminary-honors/functions.php
+++ wp-content/themes/luminary-honors/functions.php
@@ -4,10 +4,23 @@
  */
 
 
+if (!function_exists("theme_setup")) {
+    /**
+     * Sets up theme defaults and registers support for various Wordpress features.
+     * 
+     * Note that this function is hooked into the after_setup_theme hook,
+     * which runs before the init hook. The init hook is too late for some features,
+     * such as indicating support for post thumbnails
+     */
+    function theme_setup() {
+
+    }
+}
+add_action("after_setup_theme", "theme_setup");
 
  //adding our text-domain to the theme
  //need to double check if get_template_directory() looks for a specific folder name
- function theme_setup() {
+ function luminary_honors_theme_setup() {
     load_theme_textdomain("luminary-honors", get_template_directory() . "/languages");
  }
- add_action("after_setup_theme", "theme_setup");
+ add_action("after_setup_theme", "luminary_honors_theme_setup");
