de9d2b5119fb6358d2c5982543c4c752fca0ac13
Ben Klimchock <klimmy1115@gmail.com>
2024-02-02 11:13:53 -0600

    Adding theme setup function to functions.php for the text domain. Need to check on languages folder

--- wp-content/themes/luminary-honors/functions.php
+++ wp-content/themes/luminary-honors/functions.php
@@ -1,1 +1,12 @@
 <?php
+/**
+ * File for our primary functionality within our theme.
+ */
+
+
+
+ //adding our text-domain to the theme
+ function theme_setup() {
+    load_theme_textdomain("luminary-honors", get_template_directory() . "/languages");
+ }
+ add_action("after_setup_theme", "theme_setup");
