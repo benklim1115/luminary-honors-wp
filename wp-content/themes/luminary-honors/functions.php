4c877cce7885070001a8976e7703a6e915407d30
Ben Klimchock <klimmy1115@gmail.com>
2024-02-02 15:14:29 -0600

    Theme support for featured images

--- wp-content/themes/luminary-honors/functions.php
+++ wp-content/themes/luminary-honors/functions.php
@@ -32,6 +32,13 @@ if (!function_exists("theme_setup")) {
          * provide it for us.
          */
         add_theme_support("title-tag");
+
+        /**
+         * Enable support for Post Thumbnails on posts and pages.
+         * 
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support("post-thumbnails");
     }
 }
 add_action("after_setup_theme", "theme_setup");
