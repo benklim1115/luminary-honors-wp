b55ff8f0018483a94a33808c799a17010a7e2efa
Ben Klimchock <klimmy1115@gmail.com>
2024-02-02 09:56:05 -0600

    Logic to check if we have just one comment, if we have more then display that

--- wp-content/themes/luminary-honors/comments.php
+++ wp-content/themes/luminary-honors/comments.php
@@ -7,3 +7,25 @@
  if (post_password_required()) {
     return;
  }
+?>
+
+<div id="comments" class="comments-area">
+    <!--- Check if there are comments available -->
+    <?php if (have_comments()) :?>
+        <h2 class="comments-title">
+            <?php 
+                $commentCount = get_comments_number();
+                if ("1" === $commentCount) {
+                    printf(
+                        esc_html__("Comments (1)", "luminary-honors")
+                    );
+                } else {
+                    printf(
+                        esc_html__("Comments (%1$s)", "luminary-honors"),
+                        intval($commentCount)
+                    );
+                }
+            ?>
+        </h2>
+    <?php endif;?>
+</div>
