a0161f75639d2ea982e3cea90104338754594db0
Ben Klimchock <klimmy1115@gmail.com>
2024-02-05 15:36:43 -0600

    Shell set up for footer, need to use font-awesome logos eventually

--- wp-content/themes/luminary-honors/footer.php
+++ wp-content/themes/luminary-honors/footer.php
@@ -8,18 +8,26 @@
 
     <!--- Footer menu for site --->
     <footer class="footer-menu-container">
-        <div class="footer-section" style="border: 2px solid red;">
+        <div class="footer-section">
             <a href="/">
                 <img id="footer-title-img" src="<?php echo get_template_directory_uri();?>/src/assets/images/logo.svg" alt="My Custom Image">
             </a>
         </div>
 
         <div class="footer-section">
-            <p style="color: white; border: 2px solid red;">Hello</p>
+            <ul id="footer-links">
+                <li><a href="/">Register Online</a></li>
+                <li><a href="/">UT Health San Antonio</a></li>
+                <li><a href="/">UT Health San Antonio Multispecialty & Research Hospital</a></li>
+            </ul>
         </div>
 
         <div class="footer-section">
-            <p style="color: white; border: 2px solid red;">Next section</p>
+            <div class="social-media-link">1</div>
+            <div class="social-media-link">2</div>
+            <div class="social-media-link">3</div>
+            <div class="social-media-link">4</div>
+            <div class="social-media-link">5</div>
         </div>
     </footer>
     
