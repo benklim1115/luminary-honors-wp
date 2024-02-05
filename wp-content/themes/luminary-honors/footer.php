164db645186cf1c5d9787a556c71c2db092e6db8
Ben Klimchock <klimmy1115@gmail.com>
2024-02-05 12:59:36 -0600

    Header and footer appearing. Need to add some styling

--- wp-content/themes/luminary-honors/footer.php
+++ wp-content/themes/luminary-honors/footer.php
@@ -5,5 +5,17 @@
 ?>
 
     <?php wp_footer();?>
+
+    <!--- Footer menu for site --->
+    <?php 
+        wp_nav_menu(array(
+            'theme_location' => 'footer',
+            'menu_id'        => 'footer-menu',
+            'container'      => 'nav',
+            'container_class'=> 'footer-menu-container',
+            'menu_class'     => 'footer-menu-list',
+        ));
+    ?>
+    
     </body>
 </html>
--- wp-content/themes/luminary-honors/header.php
+++ wp-content/themes/luminary-honors/header.php
@@ -16,7 +16,21 @@
     <!--- we can pass in our class in body_class to override css --->
     <body class="body-main" style="margin-top: 0;"<?php body_class($css_class = "body-main");?>>
 
+    <!--- Luminary Honors menu --->
+    <?php
+        wp_nav_menu(array (
+            "theme_location" => "primary",
+            "menu_id" => "primary-menu",
+            "container" => "nav",
+            "container-class" => "primary-menu-container",
+            "menu_class" => "primary-menu-list"
+        ))
+    ?>
+    
+
+    <!--- Commenting out hard coded header for now
     <header class="main-site-header">
         <h2 style="text-align: center; color: #AD976E; padding-top: 3rem; margin-top: 0px"> PRESIDENT'S Luminary HONORS </h2>
         <p style="text-align: center; color: #AD976E;">UT Health San Antonio</p>
     </header>
+    --->
