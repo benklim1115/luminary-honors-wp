a688fd4bde80afddeacc068a1ab9380c1ddea59f
Ben Klimchock <klimmy1115@gmail.com>
2024-02-05 10:38:18 -0600

    Theme support for search/comment forms

--- wp-content/themes/luminary-honors/functions.php
+++ wp-content/themes/luminary-honors/functions.php
@@ -45,6 +45,18 @@ if (!function_exists("luminary_honors_theme_setup")) {
             "default-color" => "ffffff",
             "default-image" => "",
         )));
+
+        /**
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5
+         */
+        add_theme_support("html5", array(
+            "search-form",
+            "comment-form",
+            "comment-list",
+            "gallery",
+            "caption",
+        ));
     }
 }
 add_action("after_setup_theme", "luminary_honors_theme_setup");
