de7845384a135674fbcf9f8ea967411a728cf5f1
Ben Klimchock <klimmy1115@gmail.com>
2024-02-06 09:47:38 -0600

    Added back within div/main

--- wp-content/themes/luminary-honors/index.php
+++ wp-content/themes/luminary-honors/index.php
@@ -6,6 +6,16 @@
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-        
+        <?php 
+            get_header();
+            get_search_form();
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                    get_template_part("template-parts/post/content");
+                endwhile;
+            endif;
+            get_footer();
+        ?>
     </main>
 </div>
