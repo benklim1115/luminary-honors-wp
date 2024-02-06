3bbd7f5248a7d8c1b7aa53410ab3ec501ba51805
Ben Klimchock <klimmy1115@gmail.com>
2024-02-06 15:38:37 -0600

    Changes to footer links and socials

--- wp-content/themes/luminary-honors/footer.php
+++ wp-content/themes/luminary-honors/footer.php
@@ -22,6 +22,7 @@
             </ul>
         </div>
 
+        <!--- 
         <div class="footer-section">
             <div class="social-media-link">1</div>
             <div class="social-media-link">2</div>
@@ -29,7 +30,21 @@
             <div class="social-media-link">4</div>
             <div class="social-media-link">5</div>
         </div>
+        --->
+
+        <div class="footer-section">
+            <ul class="social">
+                <li><a href="#"><i class="fab fa-instagram-square fa-2x social-media-link"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube-square fa-2x social-media-link"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin fa-2x social-media-link"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter-square fa-2x social-media-link"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-square fa-2x social-media-link"></i></a></li>
+            </ul>
+        </div>
+
     </footer>
+
+
     
     </body>
 </html>
