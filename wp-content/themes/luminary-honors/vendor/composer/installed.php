0819fad61c4a3ab92a61089c9774f9bbc09ad373
Ben Klimchock <klimmy1115@gmail.com>
2024-02-08 12:30:15 -0600

    Composer init and dependencies we will need for the project

---
+++ wp-content/themes/luminary-honors/vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => 'web-digital-services/luminary-honors',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '679b2f09e20b4313da545b44250d67d35839ad9b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => '1.x-dev',
+            'version' => '1.9999999.9999999.9999999-dev',
+            'reference' => '894a0b5c5d34c88b69b097f2aae1439730fa6836',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'web-digital-services/luminary-honors' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '679b2f09e20b4313da545b44250d67d35839ad9b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
