27805f19142f0ca4bf61f255e8600ab8fe851a8b
Ben Klimchock <klimmy1115@gmail.com>
2024-02-09 13:47:27 -0600

    RSVP placeholder page, looking into possibly creating a dummy form for now

--- wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
@@ -5,7 +5,20 @@
  */
 ?>
 
+<!--- Need to see if we still have to put the body here or other div --->
+<!-- <div class="off-canvas-content" data-off-canvas-content> -->
+        <main class="grid-container">
+            <div class="grid-x grid-margin-x margin-top-2 margin-bottom-2">
+                <div class="cell small-12 medium-8">
+                    <h1 class="color">RSVP</h1>
+                </div>
+            </div>
 
+            <div class="grid-x grid-margin-x margin-bottom-3">
+                <!--- Content goes here --->
+                <!--- form for user to RSVP? --->
+            </div>
+        </main>
 
 
  <!--- Saw this on template keeping if we need it later
