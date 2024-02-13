ee86302498371dac9ec6d3ce30ea989bb9f98fb4
Ben Klimchock <klimmy1115@gmail.com>
2024-02-13 14:52:21 -0600

    Leaving room for formidable form

--- wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
@@ -14,19 +14,10 @@
             </div>
         </div>
 
-        <div class="grid-x grid-margin-x margin-bottom-3">
+        <div class="grid-x grid-margin-x">
             <!--- Content goes here --->
             <!--- form for user to RSVP? --->
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                deserunt mollit anim id est laborum.
-            </p>
         </div>
     </main>
 </div>
