96eea52457d6a184341e58d19448d0d63d47bdd8
Ben Klimchock <klimmy1115@gmail.com>
2024-02-13 15:08:49 -0600

    Fixing up layout for RSVP page

--- wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-rsvp.php
@@ -10,13 +10,18 @@
         <div class="grid-x grid-margin-x margin-top-2 margin-bottom-2">
             <div class="cell small-12 medium-8">
                 <h1 class="color">RSVP</h1>
+                <h2 class="color">Reserve your spot for this special night!</h2>
+                <p>
+                    Here is some info on how to fill out the form:<br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                    officia deserunt mollit anim id est laborum.
+                </p>
             </div>
-        </div>
-
-        <div class="grid-x grid-margin-x">
-            <!--- Content goes here --->
-            <!--- form for user to RSVP? --->
-
         </div>
     </main>
 </div>
