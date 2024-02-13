3d6f63e4b0974c57e8dfc917058dbce068caeaf7
Ben Klimchock <klimmy1115@gmail.com>
2024-02-13 14:32:31 -0600

    Fixed 404 page layout

--- wp-content/themes/luminary-honors/404.php
+++ wp-content/themes/luminary-honors/404.php
@@ -7,20 +7,24 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <section class="error-404 not-found">
-            <header class="page-header">
-                <h1 class="page-title"><?php esc_html_e("Oops! That page can&rsquo;t be found.", 'luminary-honors');?></h1>
-            </header>
-
-            <div class="page-content">
-                <p><?php esc_html_e("It looks like nothing was found at this location. Maybe try one of the links below or searching a different route.");?></p>
-                <?php get_search_form();?>
+    <main class="grid-container">
+        <div class="grid-x grid-margin-x margin-top-2 margin-bottom-2">
+            <div class="cell small-12 medium-12 text-center">
+                <h1 class="color margin-bottom">Oops! Page not found!</h1>
+                <p class="margin-bottom"><?php esc_html_e("It looks like nothing was found at this location. Try one of the links below!");?></p>
+                <ul style="list-style-type: none; margin-right: 2rem">
+                    <li><a href="/home" style="color: #171717;">Home</a></li>
+                    <li><a href="/sponsorships" style="color: #171717;">Sponsorships</a></li>
+                    <li><a href="/honorees" style="color: #171717;">Honorees</a></li>
+                    <li><a href="/rsvp" style="color: #171717;">RSVP</a></li>
+                </ul>
             </div>
-        </section>
+        </div>
     </main>
-</div>
+
+<script>
+    $(document).foundation();
+</script>
 
 <?php 
 get_footer();
