7ac48aacd03b9308606b25b1c5ef50746cb48e30
Ben Klimchock <klimmy1115@gmail.com>
2024-02-20 14:13:12 -0600

    Need to see what is wrong with my page.php compared to utShield

--- wp-content/themes/luminary-honors/page.php
+++ wp-content/themes/luminary-honors/page.php
@@ -7,49 +7,48 @@ get_header();
 ?>
 
 <!--- Will need to plug in Hunter"s pages here --->
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
 
-        <!--- loop to print out the page --->
-        <?php 
-            while (have_posts()) :
-                echo "<p>Where is this showing up?</p>";
-                the_post();
-                the_content();
+<main id="main" class="site-main">
 
-                // Check if we are on the / page
-                // Consider refactoring to change logic for home page to search for the root /
-                // Changing the root of the project in Settings -> Reading in wp-admin works for now
-                if (is_page("home")) :
-                    get_template_part("template-parts/page/content", "flame");
+    <?php 
+        // Check if we are on the / page
+        // Consider refactoring to change logic for home page to search for the root /
+        // Changing the root of the project in Settings -> Reading in wp-admin works for now
+        if (is_page("home")) :
+            get_template_part("template-parts/page/content", "flame");
 
-                // Check if we are on the /sponsorships page
-                elseif (is_page("sponsorships")) :
-                    get_template_part("template-parts/page/content", "sponsorships");
+        // Check if we are on the /sponsorships page
+        elseif (is_page("sponsorships")) :
+            get_template_part("template-parts/page/content", "sponsorships");
 
-                // Check if we are on the /honorees page
-                elseif (is_page("honorees")) :
-                    get_template_part("template-parts/page/content", "honorees");
+        // Check if we are on the /honorees page
+        elseif (is_page("honorees")) :
+            get_template_part("template-parts/page/content", "honorees");
 
-                // Check if we are on the /rsvp page
-                elseif (is_page("rsvp")) :
-                    get_template_part("template-parts/page/content", "rsvp");
+        // Check if we are on the /rsvp page
+        elseif (is_page("rsvp")) :
+            get_template_part("template-parts/page/content", "rsvp");
 
-                // Not on any of the slugs? Render default template for page    
-                else :
-                    get_template_part("template-parts/page/content", "page");
+        // Not on any of the slugs? Render default template for page    
+        else :
+            get_template_part("template-parts/page/content", "page");
 
-                endif;   
+        endif; 
 
-                //if we have comments show them
-                if (comments_open() || get_comments_number()) :
-                    comments_template();
-                endif;
-            endwhile;
-        ?>
-    </main>
-    <?php get_sidebar(); ?>
-</div>
+       //loop to print out the page
+        while (have_posts()) :
+            echo "<p>Where is this showing up?</p>";
+            the_post();
+            the_content();  
+            //if we have comments show them
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+        endwhile;
+    ?>
+</main>
 
 <?php
 get_footer();
+
+
