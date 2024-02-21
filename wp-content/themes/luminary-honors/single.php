5bad969ca6cc138d02b17004cc9b49da7b01abf3
Ben Klimchock <klimmy1115@gmail.com>
2024-02-21 12:06:14 -0600

    Mirroring single.php

--- wp-content/themes/luminary-honors/single.php
+++ wp-content/themes/luminary-honors/single.php
@@ -17,8 +17,25 @@ get_header();
 				}
 			?>
             <?php get_template_part("template-parts/posts/content", get_post_format()); ?>
+            <?php if (is_singular('post')){
+						echo '<section class="less-roomy"><strong>Categories: </strong><br>';
+						$post_slug = get_site_url();
+						$id = get_the_ID();
+						$categories =  get_the_category($id);
+						echo '<ul class="tags">';
+						foreach  ($categories as $category) {
+						echo '<li><a href="' . $post_slug . '/category/' . $category->slug . '">'. $category->cat_name .'</a></li>';
+						}
+						echo '</ul>';
+						echo '</section>';
+						#do_action( 'foundationpress_post_before_comments' );
+						#comments_template();
+						#do_action( 'foundationpress_post_after_comments' );
+						}
+					?>
             <?php endwhile; ?>
         </main>
+        <?php get_sidebar(); ?>
     </div>
 </div>
 
