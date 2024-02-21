1ec97c26adb4dc572a36ed93cdb93c230d0b09bb
Ben Klimchock <klimmy1115@gmail.com>
2024-02-21 14:43:35 -0600

    I think that worked, going to make the pages in wp bakery now

--- wp-content/themes/luminary-honors/page.php
+++ wp-content/themes/luminary-honors/page.php
@@ -8,27 +8,31 @@ $banner_design = get_post_meta( $post->ID, 'banner_design_key', true );
 $add_breadcrumbs = get_post_meta( $post->ID, 'add_breadcrumbs', true );
 ?>
 
-<div class="page-title grid-container">
-    <div class="grid-x grid-padding-x">
-        <div class="cell">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php 
-                    //if we are home render the home/flame template page
-                    if(is_page("home")) :
-                        get_template_part("template-parts/page/content", "flame");
-                    //if we are NOT home, render the layout for a base template page
-                    elseif(!is_page("home")) :
-                        the_title( '<h1 class="color">', '</h1>' );
-                    endif;
-                ?>
-                <?php if ( is_page() && $post->post_parent > 0 && function_exists('yoast_breadcrumb')) { 				
-                        yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-                    }
-                ?>
-            <?php endwhile; ?>
-        </div>
-    </div>
-</div>
+<main>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php 
+            //if we are home render the home/flame template page
+            if(is_page("home")) :
+                get_template_part("template-parts/page/content", "flame");
+            //if we are NOT home, render the layout for a base template page
+            elseif(!is_page("home")) :
+                echo "
+                    <main class='grid-container'>
+                        <div class='grid-x grid-margin-x margin-top-2 margin-bottom-2'>
+                            <div class='cell small-12 medium-12'>
+                                <h1 class='color'>";the_title();"</h1>
+                            </div>
+                        </div>
+                    </main>
+                ";
+            endif;
+        ?>
+        <?php if ( is_page() && $post->post_parent > 0 && function_exists('yoast_breadcrumb')) { 				
+                yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+            }
+        ?>
+    <?php endwhile; ?>
+</main>
 <main id="main-content" class="grid-container">
     <?php the_content(); ?>
 </main>
