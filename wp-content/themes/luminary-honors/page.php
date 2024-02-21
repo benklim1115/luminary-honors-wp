a7af07dcd06362b5b295f8f210f5674c9f4244d1
Ben Klimchock <klimmy1115@gmail.com>
2024-02-21 13:50:03 -0600

    Getting closer, need to have styling show up consistent on each page

--- wp-content/themes/luminary-honors/page.php
+++ wp-content/themes/luminary-honors/page.php
@@ -4,24 +4,33 @@
  */
 
 get_header(); 
+$banner_design = get_post_meta( $post->ID, 'banner_design_key', true );
+$add_breadcrumbs = get_post_meta( $post->ID, 'add_breadcrumbs', true );
 ?>
 
+<div class="page-title grid-container">
+    <div class="grid-x grid-padding-x">
+        <div class="cell">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php 
+                    //if we are home render the home/flame template page
+                    if(is_page("home")) :
+                        get_template_part("template-parts/page/content", "flame");
+                    //if we are NOT home, render the layout for a base template page
+                    elseif(!is_page("home")) :
+                        the_title( '<h1 class="color">', '</h1>' );
+                    endif;
+                ?>
+                <?php if ( is_page() && $post->post_parent > 0 && function_exists('yoast_breadcrumb')) { 				
+                        yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+                    }
+                ?>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</div>
 <main id="main-content" class="grid-container">
-    <?php 
-        // Check if we are on the / page
-        // Consider refactoring to change logic for home page to search for the root /
-        // Changing the root of the project in Settings -> Reading in wp-admin works for now
-        if (is_page("home")) :
-            get_template_part("template-parts/page/content", "flame");
-        // Not on any of the slugs? Render default template for page    
-        else :
-            get_template_part("template-parts/page/content", "page");
-        endif; 
-    ?>
-    <?php echo "Hello from page.php";?>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
+    <?php the_content(); ?>
 </main>
 <?php
 get_footer();
--- wp-content/themes/luminary-honors/template-parts/page/content-page.php
+++ wp-content/themes/luminary-honors/template-parts/page/content-page.php
@@ -4,34 +4,18 @@
  */
 ?>
 
-<!--- NEED TO EDIT THIS --->
-<!--- If we want blank template pages to show up it does so here --->
-
-
-<!--- Content is showing up twice for every element added, check into this --->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <main class="grid-container">
-        <?php the_title("<h1 class='color'>", "</h1>");?>
-        <?php echo "Hello from content-page";?>
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
 
+<!--- Content --->
+    <footer>
         <?php
-            //page thumbnail
-            if (has_post_thumbnail()) :
-                the_post_thumbnail("full");
-            endif; 
+            wp_link_pages( array(
+                "before" => "<div class='page-links'>" . esc_html__("Pages:", "luminary-honors"),
+                "after" => "</div>",
+            ));
         ?>
-
-        <!--- Content --->
-        <div class="entry-content">
-            <?php 
-                the_content();
-
-                wp_link_pages( array(
-                    "before" => "<div class='page-links'>" . esc_html__("Pages:", "luminary-honors"),
-                    "after" => "</div>",
-                ));
-            ?>
-        </div>
-
-    </main>
+    </footer>
 </article>
