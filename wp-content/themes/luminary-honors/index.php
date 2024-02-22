c5625b6cf2441787175c3435613c44d11e134fa9
Ben Klimchock <klimmy1115@gmail.com>
2024-02-22 13:02:21 -0600

    Reverting index.php changes

--- wp-content/themes/luminary-honors/index.php
+++ wp-content/themes/luminary-honors/index.php
@@ -7,25 +7,26 @@ get_header();
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-        <?php if (have_posts()) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part("template-parts/content", get_post_format() ); ?>
-            <?php endwhile; ?>
-
-            <?php elseif (is_page("home")) : ?>
-                <?php get_template_part("template-parts/page/content", "flame"); ?>
-
-            <?php else : ?>
-            <?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-            <?php endif; // End have_posts() check. ?>
-
-            <?php
-                echo paginate_links([
-                    "prev_text" => esc_html__("Prev", "luminary-honors"),
-                    "next_text" => esc_html__("Next", "luminary-honors")
-                ]);
-            ?>
+        <?php 
+        // if we want the index page to be the landing page/flame we can add that here
+        // Consider refactoring to change logic for home page to search for the root /
+        // Changing the root of the project in Settings -> Reading in wp-admin works for now
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                    get_template_part("template-parts/post/content", get_post_format());
+                    // get_template_part("template-parts/page/content", "flame");
+                endwhile;
+            
+            
+            echo paginate_links([
+                "prev_text" => esc_html__("Prev", "luminary-honors"),
+                "next_text" => esc_html__("Next", "luminary-honors")
+            ]);
+            else : 
+                get_template_part("template-parts/page/content", "none");  
+            endif;
+        ?>
     </main>
 
     <?php get_sidebar();?>
