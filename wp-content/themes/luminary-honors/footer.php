2da7873c1682079210cbb631da99a2928a0775fc
Ben Klimchock <klimmy1115@gmail.com>
2024-02-26 15:47:26 -0600

    Custom footer styling to help with issue

--- wp-content/themes/luminary-honors/footer.php
+++ wp-content/themes/luminary-honors/footer.php
@@ -5,8 +5,8 @@
 ?>
 
         <!--- Footer menu for site --->
-        <footer style="background-color: #171717">
-            <div class="grid-container full">
+        <footer class="luminary-footer">
+            <div class="grid-container">
                 <div class="grid-x grid-margin-x align-center-middle">
                     <div class="cell small-12 medium-4 small-order-3 medium-order-1">
                         <a href="/luminary-honors/"><img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo.svg" alt="President's Luminary Honors logo"></a>
