19aaf7977ceb6ba46d61a55406c4c4374cc96523
Ben Klimchock <klimmy1115@gmail.com>
2024-02-26 15:12:47 -0600

    lets just hardcode the background since this is being stubborn

--- wp-content/themes/luminary-honors/footer.php
+++ wp-content/themes/luminary-honors/footer.php
@@ -5,7 +5,7 @@
 ?>
 
         <!--- Footer menu for site --->
-        <footer>
+        <footer style="background-color: #171717">
             <div class="grid-container">
                 <div class="grid-x grid-margin-x align-center-middle">
                     <div class="cell small-12 medium-4 small-order-3 medium-order-1">
