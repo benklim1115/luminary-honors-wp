c84bd50bc24358f20969a9660632e6261d7cdc6a
Ben Klimchock <klimmy1115@gmail.com>
2024-02-26 10:57:20 -0600

    Testing header and links

--- wp-content/themes/luminary-honors/functions.php
+++ wp-content/themes/luminary-honors/functions.php
@@ -172,5 +172,7 @@ function register_theme_menus() {
         "extra-menu" => __("Extra Menu", "luminary-honors")
         )
     );
+    require_once('library/luminary-honors/customizer/logo-upload.php' );
+    require_once('library/luminary-honors/customizer/customizer-tweaks.php' );
 }
 add_action( "init", "register_theme_menus" );
