e5982ef187a3bddfafec659b921efd40dc3dbfc1
Ben Klimchock <klimmy1115@gmail.com>
2024-02-26 10:39:37 -0600

    Ok atleast the menu works now. Backtracking and adding my logic back in

--- wp-content/themes/luminary-honors/page.php
+++ wp-content/themes/luminary-honors/page.php
@@ -15,10 +15,23 @@ get_header(); ?>
         <div class="grid-x grid-padding-x">
             <div class="cell">
 				<?php while ( have_posts() ) : the_post(); ?>
-				 <?php if(!is_front_page()){
-					 	the_title( '<h1 class="color">', '</h1>' );
-						}
-					?>
+                    <?php 
+                        //if we are home render the home/flame template page
+                        if(is_front_page()) :
+                            get_template_part("template-parts/page/content", "flame");
+                        //if we are NOT home, render the layout for a base template page
+                        else :
+                            echo "
+                                <main class='grid-container'>
+                                    <div class='grid-x grid-margin-x margin-top-2 margin-bottom-2'>
+                                        <div class='cell small-12 medium-12'>
+                                            <h1 class='color'>";the_title();"</h1>
+                                        </div>
+                                    </div>
+                                </main>
+                            ";
+                        endif;
+                    ?>
 					<?php if ( is_page() && $post->post_parent > 0 && function_exists('yoast_breadcrumb')) { 				
 						yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
 						}
