02c3baec0ca33a4be0613bffc249af7ac71db6b6
Ben Klimchock <klimmy1115@gmail.com>
2024-02-27 07:22:53 -0600

    Footer change to fix layout

--- wp-content/themes/luminary-honors/footer-backup.php
+++ wp-content/themes/luminary-honors/footer-backup.php
@@ -1,25 +1,21 @@
 <?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the "off-canvas-wrap" div and all content after.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+/** 
+ * Contains the footer for our theme
+ **/
 ?>
 
-<footer <?php echo $footer_class;?>>
+        <!--- Footer menu for site --->
+        <footer>
             <div class="grid-container">
                 <div class="grid-x grid-margin-x align-center-middle">
                     <div class="cell small-12 medium-4 small-order-3 medium-order-1">
-                        <a href="#"><img src="/logo.cd13699a.svg"></a>
+                        <a href="/luminary-honors/"><img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo.svg" alt="President's Luminary Honors logo"></a>
                     </div>
                     <div class="cell small-12 medium-5 small-order-1 medium-order-2">
                         <ul class="arrow">
                             <li><a>Register Online</a></li>
                             <li><a>UT Health San Antonio</a></li>
-                            <li><a>UT Health San Antonio Multispecialty &amp; Research Hospital</a></li>
+                            <li><a>UT Health San Antonio Multispecialty & Research Hospital</a></li>
                         </ul>
                     </div>
                     <div class="cell small-12 medium-3 small-order-2 medium-order-3">
@@ -34,8 +30,7 @@
                 </div>
             </div>
         </footer>
-
-         
-<?php wp_footer(); ?>
-</body>
+    
+    <?php wp_footer();?>
+    </body>
 </html>
